625c68b09bd3efccec5c76fa34eb2311706cc84e
Kate Lupashtsian <kate.lupashtsian@gmail.com>
2020-10-17 19:07:19 +0300

    feat: implemented function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+    let strNew = "";
+   if (str[0] !== '-') {  
+  for (i = 0; i < str.length; i++) {
+    strNew = str[i] + strNew;
+}
+    return strNew;
+ } else {
+    for (i = 0; i < str.length - 1; i++) {
+    strNew = str[i+1] + strNew;
+    }
+    return strNew;
+}
 }
